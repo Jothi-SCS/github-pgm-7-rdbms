7886e978ffaff8f2d5cffa9088e3f68e6b543d98
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-21 12:07:43 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,8 +1,4 @@
--- Create the Marksheet table with:
--- RollNo, Name, Department, Marks
-
--- Insert the five given records.
-
--- Write the query to display students
--- whose Marks are greater than 80
--- in descending order of Marks.
+SELECT RollNo, Name, Department, Marks
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
